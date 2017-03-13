d8e38ca7fbfce4121f0e2f6fb0af0563936efccf
Hyde Zhang <Phoenix_520_1227@hotmail.com>
2017-03-13 17:11:48 +0000

    Adding candidate votes partition DB helper

--- spec/unit/mongdb.test.js
+++ spec/unit/mongdb.test.js
@@ -6,44 +6,79 @@
 var chai   = require('chai');
 var expect = chai.expect;
 var config = require('konfig')().service;
+var async  = require('async');
 
 // module-under-test
 var mongodb = require('../../lib/utils/mongodb');
+
+///////////////////////////////////////////////////////////////////////////////
+// Helper
+//
+/**
+ * get random user ID between 1 - 20 for testing purpose
+ * @returns {string}
+ */
+function getUserID () {
+  return "test-" + Math.floor(Math.random() * (20 - 1) + 1);
+}
+
+/**
+ * Generate given number of vote message for given candidate
+ * @param numOfVotes
+ * @param canId
+ */
+function generateVotes(numOfVotes, canId) {
+  var votes = [];
+  for (var i = 0; i < numOfVotes; ++i) {
+    votes[i] = {user: getUserID(), candidate: canId}
+  }
+  return votes;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Test suite
 //
 describe('MongoDB helper', function() {
 
-  it('should initialise mongodb with correct collection name', function (done) {
-    mongodb.initialise(function(err, result) {
-      expect(err).to.be.null;
-      // if other collection creation has error the result collection
-      // name won't be user
-      expect(result.collectionName).to.equal('user');
-      done();
+  describe('basic operations test', function () {
+
+    it('should initialise mongodb with correct collection name', function (done) {
+      mongodb.initialise(function(err, result) {
+        expect(err).to.be.null;
+        // if other collection creation has error the result collection
+        // name won't be user
+        expect(result.collectionName).to.equal('user');
+        done();
+      });
     });
+
+    it('should be able to insert single doc into mongodb', function (done) {
+      mongodb.storeVotes({user: "test-1", candidate: 2}, function(err, result) {
+        expect(err).to.be.null;
+        expect(result.length).to.equal(1);
+        done();
+      });
+    });
+
+    it('should be able to insert multiple doc into mongodb', function (done) {
+      var testDocs = [
+        {user: "test-1", candidate: 5},
+        {user: "test-2", candidate: 3}
+      ];
+      mongodb.storeVotes(testDocs, function(err, result) {
+        expect(err).to.be.null;
+        expect(result.length).to.equal(2);
+        done();
+      });
+    });
+
+    after(function(done) {
+      mongodb.deleteDocs(config.MONGODB_COL_NAME, {user: /^test-*/}, function () {
+        mongodb.deleteDocs(config.MONGODB_USER_COL_NAME, {user: /^test-*/}, done);
+      });
+    })
   });
 
-  it('should be able to insert single doc into mongodb', function (done) {
-    mongodb.storeVotes({user: "test-1", candidate: 2}, function(err, result) {
-      expect(err).to.be.null;
-      expect(result.length).to.equal(1);
-      done();
-    });
-  });
-
-  it('should be able to insert multiple doc into mongodb', function (done) {
-    var testDocs = [
-      {user: "test-1", candidate: 5},
-      {user: "test-2", candidate: 3}
-    ];
-    mongodb.storeVotes(testDocs, function(err, result) {
-      expect(err).to.be.null;
-      expect(result.length).to.equal(2);
-      done();
-    });
-  });
 
   describe('user collection update tests', function() {
     var testVotes = [
@@ -72,12 +107,49 @@ describe('MongoDB helper', function() {
           })
         })
       })
+    });
+
+    after(function(done) {
+      mongodb.deleteDocs(config.MONGODB_COL_NAME, {user: /^test-*/}, function () {
+        mongodb.deleteDocs(config.MONGODB_USER_COL_NAME, {user: /^test-*/}, done);
+      });
     })
   });
 
-  after(function(done) {
-    mongodb.deleteDocs(config.MONGODB_COL_NAME, {user: /^test-*/}, function () {
-      mongodb.deleteDocs(config.MONGODB_USER_COL_NAME, {user: /^test-*/}, done);
+  describe('aggregation test', function() {
+    before(function (done) {
+      // generate given percentage/num of votes for each candidate from random users
+      // such that candidate one has 1 vote, candidate 4 has 5 votes etc.
+      var votes = [
+        generateVotes(1, 1),
+        generateVotes(2, 2),
+        generateVotes(4, 3),
+        generateVotes(5, 4),
+        generateVotes(8, 5),
+        generateVotes(1, 1)
+      ];
+
+      // make sure all inserts complete before proceed
+      var tasks = [];
+      for (var i = 0; i < votes.length; ++i) {
+        tasks[i] = async.apply(mongodb.storeVotes, votes[i]);
+      }
+      async.series(tasks, done);
     });
-  })
+
+    it('should be able to return votes count for each candidates', function(done) {
+      mongodb.countVotes(function (err, result) {
+        if (err) return done(err);
+        expect(result.length).to.equal(5);
+        done();
+      })
+    });
+
+    after(function(done) {
+      mongodb.deleteDocs(config.MONGODB_COL_NAME, {user: /^test-*/}, function () {
+        mongodb.deleteDocs(config.MONGODB_USER_COL_NAME, {user: /^test-*/}, done);
+      });
+    });
+  });
+
 });
